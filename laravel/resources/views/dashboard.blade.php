b3a3971c6b52155a41d29cd4b036a972668f89e9
Gabriel Faria <gabriel.faria@live.com>
2022-03-26 20:23:46 -0300

    CSS listagem domínios

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg listagem-dominios">
             
             @foreach ($dominios ?? '' as $dominio)
                 <p> ID:&nbsp;{{ $dominio->id }} -
